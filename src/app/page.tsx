2928163098448a6f8f104f290d8854122c91f4b4
Rheine Tiamsic <66781778+itsrheine@users.noreply.github.com>
2026-04-03 11:52:43 -0700

    Initial Next.js app

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3x1 font-bold">
+        Home Inspection App - Test
+      </h1>
+    </main>
+  );
+}
